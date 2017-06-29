5384dbaac2576b4b26be0a86dc8b93d1e2cb9ebc
Antony Canut <canut42@gmail.com>
2017-06-29 16:27:46 +0200

    Solution

--- TestWithSandro/ConsoleApplication/Properties/AssemblyInfo.cs
+++ TestWithSandro/ConsoleApplication/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TestWithSandro")]
+[assembly: AssemblyTitle("ConsoleApplication")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TestWithSandro")]
+[assembly: AssemblyProduct("ConsoleApplication")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("55B7BE58-E755-4483-8885-1A3DF2270681")]
+[assembly: Guid("F4ACD926-AA92-4582-8AE5-59E7D5B000FF")]
 
 // Version information for an assembly consists of the following four values:
 //
--- TestWithSandro/TestWithSandro/Tests.cs
+++ TestWithSandro/TestWithSandro/Tests.cs
@@ -7,7 +7,7 @@ namespace TestWithSandro
     public class Tests
     {
         [Test]
-        public void Test1()
+        public void Test1() 
         {
             Assert.True(true);
         }
