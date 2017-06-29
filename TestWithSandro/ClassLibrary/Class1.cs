84fb93d985f53a13679f2466be6308b5c21dfe22
Antony Canut <canut42@gmail.com>
2017-06-29 17:29:26 +0200

    Solution[A

--- TestWithSandro/ClassLibrary/Class1.cs
+++ TestWithSandro/ClassLibrary/Class1.cs
@@ -32,14 +32,12 @@ namespace ClassLibrary
 
         public bool Win()
         {
-            return ((Tab[0,0] == CurrentSign && Tab[1,1] == CurrentSign && Tab[2,2] == CurrentSign) ||
-                    (Tab[0,0] == CurrentSign && Tab[1,1] == CurrentSign && Tab[2,2] == CurrentSign));
+            return CheckLine(0,0,1,1,2,2);
         }
         
         private bool CheckLine(int x, int y, int x2, int y2, int x3, int y3)
         {
-            return ((Tab[0,0] == CurrentSign && Tab[1,1] == CurrentSign && Tab[2,2] == CurrentSign) ||
-                    (Tab[0,0] == CurrentSign && Tab[1,1] == CurrentSign && Tab[2,2] == CurrentSign));
+            return (Tab[x,y] == CurrentSign && Tab[x2,y2] == CurrentSign && Tab[x3,y3] == CurrentSign);
         }
     }
 }
